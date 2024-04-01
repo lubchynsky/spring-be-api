67b0471509a9a58c2e93d71b0c258312b6ac6a34
lubchynsky <lubchynsky.v@outlook.com>
2024-04-01 11:23:09 +0200

    Initial commit with gradle and project structure

---
+++ src/main/java/com/lubchynsky/springbeapi/SpringBeApiApplication.java
@@ -0,0 +1,13 @@
+package com.lubchynsky.springbeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lubchynsky/springbeapi/SpringBeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubchynsky.springbeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
